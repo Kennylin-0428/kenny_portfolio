1881f189fdd0cbe5f0b5e100c80ab78d7c375e86
kenny <kenny19990428@gmail.com>
2023-05-06 01:44:31 +0800

    Add About jsx and About scss

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion';
+
+import { images } from '../../contsants';
 import './About.scss';
+
+const abouts = [
+  { title: 'Backend Development', description: 'I am a good backend developer.', imgUrl: images.about01 },
+  { title: 'Frontend Development', description: 'I am a good frontend developer.', imgUrl: images.about02 },
+  { title: 'SRE ', description: 'Working on becoming a sre engineer.', imgUrl: images.about03 }
+];
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>
+      I Know that <span>Good Development </span> <br />Means <span>Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) =>(
+          <motion.div
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className='app__profile-item'
+          key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title}/>
+            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
+
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
